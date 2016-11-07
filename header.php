b6c696332f49cdf5999d942a007a3c034c2d4ca9
desinficera <sailorose@live.com>
2016-11-07 16:58:00 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-
 <?php
 //  Author: Ade A, Marcel G, Jennifer K, John L,
 //  Date: 7 November 2016
@@ -7,23 +5,19 @@
 ?>
 
 <html>
-    <head>
-        <meta charset="UTF-8">
-		<title><?php echo $page_title; ?></title>
-        <link href="public/styles.css" rel="stylesheet" type="text/css"/>
-</head>
-
-<body>
-<table width="100%" cellspacing="0" cellpadding="8" border="0">
-	<tr>
-		<td id="banner">la glace</td>
-	</tr>
-	<tr>
-		<td id="navbar">
-    	<!-- This is the navigation bar -->
-        <a href="index.php">Home</a> || <a href="menu.php">Menu</a> || <a href="login.php">Add user</a>
-    	<hr/>
-		</td>
-	</tr>
-	<tr>
-		<td>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title>
+			<?php echo $title ?>
+		</title>
+		<link type="text/css" rel="stylesheet" href="www/css/style.css"/>
+	</head>
+	<body>
+		<div id="wrapper">
+<!-- navigation bar -->
+	<div id="navbar">
+		<a href="index.php">Home<a/> || <a href="menu.php">Our Menu</a> || <a href="contact.php">Contact Us</a>
+			</div>
+	</div>
+	</body>
+</html>
