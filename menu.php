d89cce5c27fb14837a2bc7a6c402cf0928e3c9e6
desinficera <sailorose@live.com>
2016-11-07 16:23:50 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -13,6 +13,16 @@ require('includes/header.php');
 <!-- page specific content begins -->
 <h2>Our Menu</h2>
 <p>Feel free to click on the product names to learn more about the flavor!</p>
+<!-- insert hyperlinks -->
+  <a href="icdetails.php?title=Strawberry Ice Cream&image=strawberry.jpg">Strawberry Ice Cream</a>
+  <a href="icdetails.php?title=Chocolate Cookies&image=chocolatecookies.jpg">Chocolate Cookies</a>
+  <a href="icdetails.php?title=Cappuccino&image=cappuccino.jpg">Cappuccino</a>
+  <a href="icdetails.php?title=French Vanilla&image=frenchvanilla.jpg">French Vanilla</a>
+  <a href="icdetails.php?title=Rocky Road&image=rockyroad.jpg">Rocky Road</a>
+<!-- create variables -->
+<?php
+
+?>
 <!-- page specific content ends -->
 
 <?php
