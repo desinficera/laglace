e36a6cb83968df0cbd894a42eba1289af3c83ff9
desinficera <sailorose@live.com>
2016-11-07 16:09:55 -0500

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,12 @@
+<?php
+//  Author: Ade A, Marcel G, Jennifer K, John L
+//  Date: 7 November 2016
+//  Description: Menu page
+
+//initialize title variable
+$page_title = "Our Menu"
+
+//link to external header file
+require('includes/header.php');
+?>
+
