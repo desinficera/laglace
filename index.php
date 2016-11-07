ba37c974ccdaa9d7f2747aac6ba9c477e36ac6ed
desinficera <sailorose@live.com>
2016-11-07 16:05:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,30 @@
 //  Date: 7 November 2016
 //  Description: Home page of the web site
 
-//establish the variables
+//initiate the title variable
 $page_title = "la glace";
 
+//link to external header file
+require("includes/header.php")
 ?>
 
+<!-- page specific content begins -->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>la glace</title>
     </head>
     <body>
+        <h2>welcome to la glace</h2>
+        <p>it's all good, you've found the right place. la glace caters to everyone. we offer vegan, gluten free, lactose free, and fat free options. so head on over to our menu and take a look for yourself! if you have any suggestions for ice cream flavors that fit your dietary needs, please visit our "contact us" page. enjoy!</p>
         <?php
         // put your code here
         ?>
     </body>
 </html>
+<!-- page specific content ends -->
+
+<?php
+//link to external footer file
+require("includes/footer.php");    
+?>
